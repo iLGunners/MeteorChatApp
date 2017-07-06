afa98637275906fb71ea95bb54336471f7f28242
Mujtaba Nowshad <m.nowshad@eongroup.net.bd>
2017-07-06 10:15:55 +0600

    step 6.18, complete

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { NavParams } from 'ionic-angular';
-import { Chat, Message } from 'api/models';
+import { Chat, Message, MessageType } from 'api/models';
 import { Observable } from 'rxjs';
 import { Messages } from 'api/collections';
+import { MeteorObservable } from 'meteor-rxjs';
  
 @Component({
   selector: 'messages-page',
   templateUrl: 'messages.html'
 })
-export class MessagesPage implements OnInit {
+export class MessagesPage implements OnInit, OnDestroy {
   selectedChat: Chat;
   title: string;
   picture: string;
   messages: Observable<Message[]>;
+  message: string = '';
+  autoScroller: MutationObserver;
+  scrollOffset = 0;
  
-  constructor(navParams: NavParams) {
+  constructor(
+    navParams: NavParams,
+    private el: ElementRef
+    ) {
     this.selectedChat = <Chat>navParams.get('chat');
     this.title = this.selectedChat.title;
     this.picture = this.selectedChat.picture;
   }
+
+  private get messagesPageContent(): Element {
+    return this.el.nativeElement.querySelector('.messages-page-content');
+  }
+ 
+  private get messagesList(): Element {
+    return this.messagesPageContent.querySelector('.messages');
+  }
+ 
+  private get scroller(): Element {
+    return this.messagesList.querySelector('.scroll-content');
+  }
  
   ngOnInit() {
+    this.autoScroller = this.autoScroll();
     let isEven = false;
  
     this.messages = Messages.find(
@@ -33,6 +53,51 @@ export class MessagesPage implements OnInit {
       });
  
       return messages;
+    });
+  }
+
+  ngOnDestroy() {
+    this.autoScroller.disconnect();
+  }
+ 
+  autoScroll(): MutationObserver {
+    const autoScroller = new MutationObserver(this.scrollDown.bind(this));
+ 
+    autoScroller.observe(this.messagesList, {
+      childList: true,
+      subtree: true
+    });
+ 
+    return autoScroller;
+  }
+ 
+  scrollDown(): void {
+    // Scroll down and apply specified offset
+    this.scroller.scrollTop = this.scroller.scrollHeight - this.scrollOffset;
+    // Zero offset for next invocation
+    this.scrollOffset = 0;
+  }
+
+  onInputKeypress({ keyCode }: KeyboardEvent): void {
+    if (keyCode === 13) {
+      this.sendTextMessage();
+    }
+  }
+ 
+  sendTextMessage(): void {
+    // If message was yet to be typed, abort
+    if (!this.message) {
+      return;
+    }
+ 
+    MeteorObservable.call(
+      'addMessage', 
+      MessageType.TEXT,
+      this.selectedChat._id,
+      this.message
+    ).zone().subscribe(() => {
+      // Zero the input field
+      this.message = '';
     });
   }
 }
